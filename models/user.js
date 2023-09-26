ed079786e4ec01568f349cec363d7548e5e1418f
Emilien39110 <emilien.boitouzet@hotmail.fr>
2023-09-26 13:27:47 +0200

    Model de la db

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+//const db = require('../config/db');
+
+
+const userSchema = new mongoose.Schema({
+  nom: String,
+  prenom: String,
+  filiere: String,
+});
+
+module.exports = mongoose.model('User', userSchema);
