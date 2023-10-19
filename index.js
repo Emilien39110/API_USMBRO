0fb8535311763d0c12b9f3b0c6ec12e97686b311
Andrien Montmayeur <92160718+AndrienMont@users.noreply.github.com>
2023-10-19 16:28:25 +0200

    ajout socketio

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 const apiRoute = require('./routes/api');
 //const bodyParser = require('body-parser');
+const io = require('socket.io')();
 
 app.use(express.json());
 
 app.use('/api', apiRoute);
+
+io.on('connection', (socket) => {
+    console.log('New user inbound.');
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
