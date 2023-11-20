0e9978078a103bf0ae0d076e5ac3206bff546eb5
Andrien Montmayeur <92160718+AndrienMont@users.noreply.github.com>
2023-11-20 22:40:19 +0100

    MAJ server pour sockets

--- index.js
+++ index.js
@@ -2,17 +2,34 @@ const express = require('express');
 const app = express();
 const apiRoute = require('./routes/api');
 //const bodyParser = require('body-parser');
-const io = require('socket.io')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.json());
 
 app.use('/api', apiRoute);
 
-io.on('connection', (socket) => {
+io.on('connection', function(socket) {
     console.log('New user inbound.');
+
+    socket.on('salut', (message) => {
+        console.log(message);
+    });
+
+
+    socket.on('disconnect', function() {
+        console.log('User disconnected.');
+    });
+    
 });
 
+
+
+
 const PORT = process.env.PORT || 3000;
+const WS_PORT = 8080;
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
+    io.listen(WS_PORT);
+    console.log(`Websocket server started on port ${WS_PORT}`);
 });
